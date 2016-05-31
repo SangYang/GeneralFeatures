82bdc95939c87cb1451e72b03a0a8f3f57aaa069
SangYang <yssh28@gmail.com>
2016-05-31 17:01:48 +0800

    mod base64

--- base64/base64.cpp
+++ base64/base64.cpp
@@ -56,7 +56,7 @@ static int GetBase64Size(const int source_size) {
 	return base64_size;
 }
 
-int Base64Encode(const char *source, int source_size, char *base64, int base64_size) {
+int Base64Encode(const unsigned char *source, int source_size, char *base64, int *base64_size) {
 	int src_index;
 	int bas_index;
 	int cycle_count;
@@ -65,18 +65,21 @@ int Base64Encode(const char *source, int source_size, char *base64, int base64_s
 	unsigned int big_endian;
 	unsigned int little_endian;
 
-	if (!source || !base64) 
+	if (!source || !base64) {
 		return 0;
-	else
-		base64_size_min = GetBase64Size(source_size);
-	if (base64_size < base64_size_min)
-		return 0;
+	}
 	else {
-		base64[base64_size_min] = 0;
-		cycle_count = source_size / 3;
-		remainder_count = source_size % 3;
-		src_index = 0;
-		bas_index = 0;
+		base64_size_min = GetBase64Size(source_size);
+		if (*base64_size < base64_size_min)
+			return -1;
+		else {
+			*base64_size = base64_size_min;
+			base64[base64_size_min] = 0;
+			cycle_count = source_size / 3;
+			remainder_count = source_size % 3;
+			src_index = 0;
+			bas_index = 0;
+		}
 	}
 	for (; src_index < cycle_count; src_index++, bas_index++) {
 		little_endian = *(unsigned int*)(source + src_index * 3);
@@ -105,7 +108,7 @@ int Base64Encode(const char *source, int source_size, char *base64, int base64_s
 	return 1;
 }
 
-int Base64Decode(const char *base64, int base64_size, char *source, int source_size) {
+int Base64Decode(const char *base64, int base64_size, unsigned char *source, int *source_size) {
 	int bas_index;	
 	int src_index;
 	int cycle_count;
@@ -118,16 +121,17 @@ int Base64Decode(const char *base64, int base64_size, char *source, int source_s
 
 	if (!base64 || !source) 
 		return 0;
-	else
-		source_size_min = base64_size / 4 * 3;
-	if (source_size < source_size_min + 1)
-		return 0;
 	else {
-		bas_index = 0;		
-		src_index = 0;
-		cycle_count = base64_size / 4;
-		remainder_count = base64_size % 4;
-		done = false;
+		source_size_min = base64_size / 4 * 3;
+		if (*source_size < source_size_min + 1)
+			return -1;
+		else {
+			bas_index = 0;		
+			src_index = 0;
+			cycle_count = base64_size / 4;
+			remainder_count = base64_size % 4;
+			done = false;
+		}
 	}
 	for (; bas_index < cycle_count; bas_index++, src_index++) {
 		if (BASE64_END_CHAR == base64[bas_index*4+0])
@@ -180,6 +184,7 @@ int Base64Decode(const char *base64, int base64_size, char *source, int source_s
 	if (remainder_count > 0 && !done) {
 		source[src_index*3+0] = 0;
 	}
+	*source_size = source_size_min - end_char_count;
 	return 1;
 }
 
@@ -187,16 +192,18 @@ int Base64Decode(const char *base64, int base64_size, char *source, int source_s
 
 #if 0
 int main() {
-	char one[] = "abc";
-	int  one_size;
+	char one[] = "12345";
+	int one_size;
+	int  one_basesize = 256;
 	char one_base64[256];
 	char one_base64_len;
 	char two[256];
+	int two_size = 256;
 
 	one_size = strlen(one);
-	Base64Encode(one, one_size, one_base64, 256);	
-	one_base64_len = strlen(one_base64) + 1;
-	Base64Decode(one_base64, one_base64_len, two, 256);
+	Base64Encode((unsigned char *)one, one_size, one_base64, &one_basesize);	
+	one_base64_len = strlen(one_base64);
+	Base64Decode(one_base64, one_base64_len, (unsigned char *)two, &two_size);
 	printf("%s\n", two);
 
 	FILE *photo;
@@ -206,9 +213,9 @@ int main() {
 	int  photo_base64_len;
 	FILE *photo_two;
 	char *photo_two_buf;
-	int  photo_two_buf_len;
+	int  photo_two_buf_len = 1024*1024;
 
-	photo = fopen("E:\\5_Photo\\images\\3.jpg", "rb");
+	photo = fopen("E:\\5_Photo\\images\\11.jpg", "rb");
 	fseek(photo, 0, SEEK_END);
 	photo_buf_size = ftell(photo);
 	fseek(photo, 0, SEEK_SET);
@@ -218,11 +225,11 @@ int main() {
 	photo_base64_len = photo_buf_size / 3 * 4 + 4;
 	photo_base64 = (char*)malloc(photo_base64_len+1);
 	photo_base64[photo_base64_len] = 0;
-	Base64Encode(photo_buf, photo_buf_size, photo_base64, photo_base64_len);
+	Base64Encode((unsigned char *)photo_buf, photo_buf_size, photo_base64, &photo_base64_len);
 	
 	photo_two_buf = (char*)malloc(1024*1024+1);
 	photo_two_buf[1024*1024] = 0;
-	Base64Decode(photo_base64, photo_base64_len, photo_two_buf, 1024*1024);
+	Base64Decode(photo_base64, photo_base64_len, (unsigned char *)photo_two_buf, &photo_two_buf_len);
 	photo_two_buf_len = photo_buf_size;
 	photo_two = fopen("123.jpg", "wb");
 	fwrite(photo_two_buf, sizeof(char), photo_two_buf_len, photo_two);
